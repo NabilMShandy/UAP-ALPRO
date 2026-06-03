37dbdcdd8ddf749f59383b34dff2385cdff23f1b
NabilMShandy <nmuhammadshandy@gmail.com>
2026-06-03 19:20:47 +0700

    .

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from login_user import start_program
-from dfss import dfs, goa, harta, node_acak
+from dfss import dfs, goa
+from bfss import Bfs
 if __name__ == "__main__":
     # start_program()
-    dfs(goa, "Goa Tengkorak")
+    
+    
+    # dfs(goa, "Goa Tengkorak")
+    Bfs(goa, "Goa Tengkorak")
