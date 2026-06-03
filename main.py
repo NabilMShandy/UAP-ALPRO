57127d36f0b55ce10832df951cee03eaf617a9ab
NabilMShandy <nmuhammadshandy@gmail.com>
2026-06-03 19:43:43 +0700

    .

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from login_user import start_program
+from login_user import start_program, tampilkan_banner
 from dfss import dfs, goa
 from bfss import Bfs
 import time
+
 if __name__ == "__main__":
     while True:
-        start_program()
+        tampilkan_banner()
+        time.sleep(2)
+        # start_program()
         
-        print("\n" + "="*50)
+        print("="*50)
         print(f" INVESTIGASI GUA MISTERIUS - USER:")
         print("1. Cari Harta Karun dengan metode DFS (Depth First Search)")
         print("2. Cari Harta Karun dengan metode BFS (Breadth First Search)")
@@ -15,11 +18,13 @@ if __name__ == "__main__":
         pilihan = input("Pilih metode (1/2/3): ").strip()
         
         if pilihan == "1":
-            print("\n MEMULAI PENELUSURAN DFS")
+            print("\n 🔍 MEMULAI PENELUSURAN DFS 🔍")
             dfs(goa, "Goa Tengkorak")
+            
         elif pilihan == "2":
-            print("\n MEMULAI PENELUSURAN BFS")
+            print("\n 🌿 MEMULAI PENELUSURAN BFS 🌿")
             Bfs(goa, "Goa Tengkorak")
+            
         elif pilihan == "3":
             print("Menghentikan program...")
             time.sleep(3)
